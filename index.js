005d37f7837646635ec9cca3ea780a842c39713d
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-27 18:14:24 -0400

    added required packages, updated comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// TODO: Include packages needed for this application
+// Required Packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Questions array for user input
 const questions = [];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {}
 
 // Function call to initialize app
