a2a975d5fd9137ece62baedc64eb558fbf849f80
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-28 18:19:29 -0400

    edited comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
   if (license === 'Apache 2.0') {
     return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
@@ -16,18 +16,18 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
+// If there is no license, returns an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
+// If there is no license, returns an empty string
 function renderLicenseSection(license) {
   (license === 'None') ? '' : `## License
   This project is licensed under the terms of the ${data.license} license.`;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
