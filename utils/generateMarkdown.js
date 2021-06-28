7e788fbd02b91e32cae663f43cc856a2441840ae
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-28 15:25:45 -0400

    added if statements for mit and no licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,7 +9,11 @@ function renderLicenseBadge(license) {
     return `![License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)`;
   } else if (license === 'GLP 3.0') {
     return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
-  } 
+  } else if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license link
