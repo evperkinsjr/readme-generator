a3c1f236254f120a8c722b0949ee673cd6c9abaa
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-06-28 15:56:50 -0400

    updated functions that returns the license section to the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,10 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  (license === 'None') ? '' : `## License
+  This project is licensed under the terms of the ${data.license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
